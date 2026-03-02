7f079d5f11943934658df9be60e14bef2c64281a
stephanie rousset <strabon@club-internet.fr>
2026-03-02 10:50:56 +0100

    fix: add name in export  action

--- app/controllers/decidim/term_customizer/admin/translations_controller.rb
+++ app/controllers/decidim/term_customizer/admin/translations_controller.rb
@@ -85,7 +85,8 @@ module Decidim
         def export
           enforce_permission_to :export, :translation_set, translation_set: set
 
-          ExportJob.perform_later(current_user, set, params[:format] || "json")
+          name = "set-translations"
+          ExportJob.perform_later(current_user, set, name, params[:format] || "json")
 
           flash[:notice] = I18n.t("exports.notice", scope: "decidim.admin")
 
